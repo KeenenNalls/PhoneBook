24d12b08ab636e182b2e3bc872d438ef1841d95e
Keenen <keenen@Zipcoders-MacBook-Pro.local>
2023-08-03 08:32:40 -0400

    all test passed

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -9,67 +9,58 @@ import java.util.HashMap;
  * Made WAY better by kristofer 6/16/20
  */
 public class PhoneBook {
-
     private final Map<String, List<String>> phonebook;
-
     public PhoneBook(Map<String, List<String>> map) {
         this.phonebook = map;
     }
 
     public PhoneBook() {
+        //this(new TreeMap<>());
         this(new LinkedHashMap<>());
     }
 
     public void add(String name, String phoneNumber) {
-        if (phonebook.containsKey(name)) {
-            phonebook.get(name).add(phoneNumber);
-        }else{
-            phonebook.put(name, new ArrayList<>());
-            phonebook.get(name).add(phoneNumber);
+        if (this.phonebook.containsKey(name)) {
+            this.phonebook.get(name).add(phoneNumber);
+        } else {
+            List<String> numberList = new ArrayList<>();
+            numberList.add(phoneNumber);
+            this.phonebook.put(name, numberList);
         }
     }
-
-
     public void addAll(String name, String... phoneNumbers) {
-        for(String s: phoneNumbers){
-            add(name, s);
+        for(String number : phoneNumbers){
+            add(name, number);
         }
     }
-
-
     public void remove(String name) {
-        phonebook.remove(name);
+        this.phonebook.remove(name);
     }
-
+    public Boolean hasEntry(String name){
+        return this.phonebook.containsKey(name);
+    }
     public Boolean hasEntry(String name, String phoneNumber) {
-        return phonebook.containsKey(name);
+        return this.phonebook.containsKey(name) &&
+                this.phonebook.get(name).contains(phoneNumber);
     }
-
     public List<String> lookup(String name) {
-        return phonebook.get(name);
+        return this.phonebook.get(name);
     }
-
     public String reverseLookup(String phoneNumber)  {
-        for(String key: phonebook.keySet()) {
-
-            if(hasEntry(key, phoneNumber)){
-                return key;
+        for(String check : phonebook.keySet()){
+            if(hasEntry(check, phoneNumber)){
+                return check;
             }
         }
         return null;
     }
-
-
     public List<String> getAllContactNames() {
-        ArrayList<String> b = new ArrayList<>();
-        for (String n : phonebook.keySet()){
-            b.add(n);
-        }
-        return b;
+        List<String> newList = new ArrayList<>();
+        for (String name : phonebook.keySet())
+            newList.add(name);
+        return newList;
     }
-
-
     public Map<String, List<String>> getMap() {
-        return new HashMap(phonebook);
+        return new HashMap<>(this.phonebook);
     }
 }
